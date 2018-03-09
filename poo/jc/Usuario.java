10606be7cb0f28eddb44faf136f3a77e304ce092
AlfredoVargas9226 <fredi_9226@hotmail.com>
2018-03-08 19:24:13 -0600

    Add files via upload

---
+++ poo/jc/Usuario.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo.jc;
+
+/**
+ *
+ * @author rapid
+ */
+public class Usuario {
+    //Regla 1 del encapsulamiento
+   private float peso;
+   private float altura;
+
+    /**
+     * @return the peso
+     */
+    public float getPeso() {
+        return peso;
+    }
+
+    /**
+     * @param peso Este es el valor del peso que debes pasar al metodo
+     */
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    /**
+     * @return the altura
+     */
+    public float getAltura() {
+        return altura;
+    }
+
+    /**
+     * @param altura the altura to set
+     */
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+   
+   
+}
